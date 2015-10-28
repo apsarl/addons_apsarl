5bf7b93cf3c95568e64a55bfc744cee118089799
Richmond FIKO <richmondfiko@africaperformances-ci.com>
2015-10-28 08:11:02 +0000

    commit 28102015

--- l10n_syscohada_ap/__openerp__.py
+++ l10n_syscohada_ap/__openerp__.py
@@ -24,14 +24,12 @@
     'version' : '1.0',
     'author' : 'Africa Performances',
     'category' : 'Localization/Account Charts',
-    'description': """
-================================================================================
-Accounting chart for SYSCOHADA area according to the 2014 - 2015 new standards.
-- Account type from Annual Financial Statement
-- New Accounts
-- All VAT Taxes
-================================================================================
-    """,
+    'description': 	"""
+					Accounting chart for SYSCOHADA area according to the 2014 - 2015 new standards.
+					- Account type from Annual Financial Statement
+					- New Accounts
+					- All VAT Taxes
+					""",
     'website': 'http://www.africaperformances-ci.com/',
     'depends' : ['account', 'base_vat'],
     'demo' : [],

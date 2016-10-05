05c5596f6fe3e03e325699ace03f3c5a6dfa74f7
Richmond FIKO <richmondfiko@africaperformances-ci.com>
2016-10-05 16:21:27 +0000

    Launch mfi and insurance coa

---
+++ l10n_cima_insurance/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 AFRICA PERFORMANCES (<www.africaperformances-ci.com>).
+#    contact: infos@africaperformances-ci.com
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name' : 'CIMA INSURANCE CHART OF ACCOUNTS - 2014',
+    'version' : '1.0',
+    'author' : 'Africa Performances',
+    'category' : 'Localization/Account Charts',
+    'description': 	"""
+					- Accounting chart CIMA for Insurance companies according to the 2014 revision (to do)
+					- Account type from Annual Financial Statement (to do)
+					""",
+    'website': 'http://www.africaperformances-ci.com/',
+    'depends' : ['account', 'base_vat'],
+    'demo' : [],
+    'data' : [],
+	'auto_install': False,
+    'installable': False,
+    'active': False,
+    'application': False,	
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
